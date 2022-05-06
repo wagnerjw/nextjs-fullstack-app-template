c75be3fd050c5dcedd39a86dada6dc4965485cde
Julian <julian@wagnerjw.dev>
2022-05-05 21:16:53 -0700

    feat: create CatCard

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { RouterContext } from 'next/dist/shared/lib/router-context';
 import * as NextImage from 'next/image';
-import '../src/styles/globals.css';
+// import '../../styles/globals.css';
 // import { AuthProvider } from '../state/auth/AuthContext';
 
 const BREAKPOINTS_INT = {
@@ -35,6 +35,14 @@ Object.defineProperty(NextImage, 'default', {
   configurable: true,
   value: (props) => <OriginalNextImage {...props} unoptimized />,
 });
+
+// export const decorators = [
+//   (Story) => (
+//     <AuthProvider>
+//       <Story />
+//     </AuthProvider>
+//   ),
+// ];
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
